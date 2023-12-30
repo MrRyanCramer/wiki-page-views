5fb61ed315c431db05b48bb412222b6bed592e75
rcramer <mrryancramer@gmail.com>
2023-12-29 21:33:19 -0500

    Added documentation for date_util

--- app/date_util.py
+++ app/date_util.py
@@ -5,6 +5,10 @@ from app.exceptions import ValidationError
 
 
 def get_month_range(year: int, month: int) -> Tuple[datetime.date, datetime.date]:
+    """Returns a tuple containing the first day and last day for a given month
+
+    Raises :class:`ValidationError`
+    """
     try:
         last_day_number_for_month = calendar.monthrange(year, month)[1]
         return (
@@ -16,6 +20,10 @@ def get_month_range(year: int, month: int) -> Tuple[datetime.date, datetime.date
 
 
 def get_week_range(year: int, week: int) -> Tuple[datetime.date, datetime.date]:
+    """Returns a tuple containing the first day and last day for a given week
+
+    Raises :class:`ValidationError`
+    """
     try:
         return (
             datetime.date.fromisocalendar(year, week, 1),
@@ -26,11 +34,13 @@ def get_week_range(year: int, week: int) -> Tuple[datetime.date, datetime.date]:
 
 
 def verify_date_in_past(date: datetime.date):
+    """Raises a :class:`ValidationError` if the date is not in the past"""
     if date > datetime.date.today():
         raise ValidationError("Date information provided must be in the past")
 
 
 def get_dates_for_week(year: int, week: int) -> List[datetime.date]:
+    """Gets a list containing all dates in a given week"""
     dates = []
     for day in range(1, 8):
         dates.append(datetime.date.fromisocalendar(year, week, day))
